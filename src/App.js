7d9ba7001314f9eb6331428a684d4b9f9691403f
Andrew <justkrut514@gmail.com>
2021-03-01 10:25:41 -0500

    Refactor insertion logic

--- src/App.js
+++ src/App.js
@@ -13,41 +13,19 @@ function App() {
     const [events, setEvents] = useState(presetEvents);
 
     const insertionNeeded = (breakDate) => {
-        let sameDateEvents = events.filter((ev) =>
-            moment(breakDate).isSame(moment(ev.start), "day")
-        );
+        let sameDateEvents = getEvents(breakDate);
+        let [breakStart] = getBreakTiming(sameDateEvents);
+        return breakStart != null;
+    };
 
-        sameDateEvents.sort((e1, e2) =>
-            moment(e1.start).isBefore(moment(e2.start)) ? -1 : 1
-        );
-        let workStarted = null,
-            workEnded = null;
-        let breakNeeded = false;
-        sameDateEvents.forEach((element) => {
-            if (!element.isABreak) {
-                if (!workStarted) {
-                    workStarted = moment(element.start);
-                    workEnded = moment(element.end);
-                } else {
-                    if (workEnded.isSame(moment(element.start)))
-                        workEnded = moment(element.end);
-                    else {
-                        workStarted = moment(element.start);
-                        workEnded = moment(element.end);
-                    }
-                }
-            } else {
-                if (workStarted) {
-                    let workLength = workEnded.diff(workStarted, "hours", true);
-                    if (workLength > 1.5) {
-                        breakNeeded = true;
-                        return;
-                    }
-                    workStarted = workEnded = null;
-                }
-            }
-        }); // first work event
-        return breakNeeded;
+    const insertBreaks = (breakDate) => {
+        let sameDateEvents = getEvents(breakDate);
+        let [breakStart, breakEnd] = getBreakTiming(sameDateEvents);
+        while (breakStart != null) {
+            console.log(`found break to insert: ${breakStart}, ${breakEnd}`);
+            /* insert breaks */
+            [breakStart, breakEnd] = [null, null] /* getBreakTiming() */;
+        }
     };
 
     return (
@@ -93,7 +71,7 @@ function App() {
                         onClick={() => {
                             if (modeSelected === "d") {
                                 if (insertionNeeded(date)) {
-                                    console.log("inserted breaks");
+                                    insertBreaks(date);
                                 } else {
                                     console.log("all good");
                                 }
@@ -113,6 +91,60 @@ function App() {
             </main>
         </div>
     );
+
+    function diffHours(workEnded, workStarted) {
+        return workEnded.diff(workStarted, "hours", true);
+    }
+
+    function getEvents(breakDate) {
+        let sameDateEvents = events.filter((ev) =>
+            moment(breakDate).isSame(moment(ev.start), "day")
+        );
+
+        sameDateEvents.sort((e1, e2) =>
+            moment(e1.start).isBefore(moment(e2.start)) ? -1 : 1
+        );
+        return sameDateEvents;
+    }
+
+    function getBreakTiming(eventArray) {
+        let workStart = null,
+            workEnd = null;
+        let breakStart = null,
+            breakEnd = null;
+        eventArray.some((element) => {
+            if (!element.isABreak) {
+                if (!workStart) {
+                    workStart = moment(element.start);
+                    workEnd = moment(element.end);
+                } else {
+                    if (workEnd.isSame(moment(element.start))) {
+                        workEnd = moment(element.end);
+                    } else {
+                        workStart = moment(element.start);
+                        workEnd = moment(element.end);
+                    }
+                }
+                let workLength = diffHours(workEnd, workStart);
+                console.log(
+                    `cur ev: ${element.title}, workLength > 1.5: ${
+                        workLength > 1.5
+                    }`
+                );
+                if (workLength > 1.5) return true;
+            } else {
+                if (workStart) {
+                    let workLength = diffHours(workEnd, workStart);
+                    if (workLength > 1.5) return true;
+                    workStart = workEnd = null;
+                }
+            }
+            return false;
+        });
+        breakStart = workEnd;
+        breakEnd = breakStart ? moment(breakStart).add(20, "minutes") : null;
+        return [breakStart, breakEnd];
+    }
 }
 
 export default App;
